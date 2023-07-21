c743f7ba5ac7a3c58f12edfe7e0d4d801d78c9c2
Pjenonghun <133840764+Pjenonghun@users.noreply.github.com>
2023-07-21 18:06:01 +0900

    Update jh_reviewDTO.java

--- Digital_Oasis/Review/jh_reviewDTO.java
+++ Digital_Oasis/Review/jh_reviewDTO.java
@@ -7,24 +7,4 @@ public class jh_reviewDTO {
 	String mid;
 	int review_rate;
 	String review_content;
-	public String getMid() {
-		return mid;
-	}
-	public void setMid(String mid) {
-		this.mid = mid;
-	}
-	public int getReview_rate() {
-		return review_rate;
-	}
-	public void setReview_rate(int review_rate) {
-		this.review_rate = review_rate;
-	}
-	public String getReview_content() {
-		return review_content;
-	}
-	public void setReview_content(String review_content) {
-		this.review_content = review_content;
-	}
-	
-	
 }
